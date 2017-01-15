d4f681371eb6fb1141011fdf2a50bf23e7b93f1e
Saggaf Arsyad <saggaf.arsyad@gmail.com>
2017-01-15 20:34:44 +0700

    Simple Insert and Query script to database

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "time"
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 // ChartData struct
 type ChartData struct {
@@ -10,4 +16,39 @@ type ChartData struct {
 }
 
 func main() {
+	// Open database
+	db, err := sql.Open("mysql", "root:root@/virtualshield_demo?parseTime=true")
+	checkErr(err)
+	// Close database on finished
+	defer db.Close()
+	// Prepare statement
+	stmt, err := db.Prepare("INSERT `virtualshield_demo`.`data_mq135` SET data=?")
+	checkErr(err)
+	// Execute statement
+	res, err := stmt.Exec(1)
+	checkErr(err)
+	// Get Id of latest data
+	id, err := res.LastInsertId()
+	checkErr(err)
+	// Query data
+	stmt, err = db.Prepare("SELECT * FROM data_mq135 WHERE id=?")
+	checkErr(err)
+	rows, err := stmt.Query(id)
+	checkErr(err)
+	// Scan rows
+	for rows.Next() {
+		var id int32
+		var data float32
+		var created time.Time
+		err = rows.Scan(&id, &data, &created)
+		checkErr(err)
+		row := ChartData{id, data, created}
+		fmt.Println(row)
+	}
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
